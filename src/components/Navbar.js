d886f55507887a1916836cf305ee68d6ce8f1c4a
Hubert Marciniak <hubert.mar@icloud.com>
2025-06-18 22:23:22 +0200

    add logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,39 @@ const linkClass = ({ isActive }) =>
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <img 
+          src="/assets/logo_transparent.png" 
+          alt="Logo" 
+          className={styles.logo} 
+        />
+      </div>
       <ul>
-        <li><NavLink to="/" end className={linkClass}>Home</NavLink></li>
-        <li><NavLink to="/bonus-optimizer" className={linkClass}>Bonus Optimizer</NavLink></li>
-        <li><NavLink to="/arbitrage-calculator" className={linkClass}>Arbitrage Calculator</NavLink></li>
-        <li><NavLink to="/margin-checker" className={linkClass}>Margin Checker</NavLink></li>
-        <li><NavLink to="/contact" className={linkClass}>Contact</NavLink></li>
+        <li>
+          <NavLink to="/" end className={linkClass}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/bonus-optimizer" className={linkClass}>
+            Bonus Optimizer
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/arbitrage-calculator" className={linkClass}>
+            Arbitrage Calculator
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/margin-checker" className={linkClass}>
+            Margin Checker
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={linkClass}>
+            Contact
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
